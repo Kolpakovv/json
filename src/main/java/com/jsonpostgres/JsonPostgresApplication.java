3e3de2bd719b7cfd087c85b5b1b2214907073eec
vasya <vasya_97@bk.ru>
2018-07-12 19:47:37 +0300

    Mitya

--- src/main/java/com/jsonpostgres/JsonPostgresApplication.java
+++ src/main/java/com/jsonpostgres/JsonPostgresApplication.java
@@ -1,4 +1,4 @@
-package com.kirillch.jsonpostgres;
+package com.jsonpostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
